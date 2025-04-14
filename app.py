e88c505ef56334096e64d7f6a42f9606ce6205c0
Muhammad Akbar Abdul Rehman <akbarabdulrehman25@gmail.com>
2025-04-14 14:53:45 -0700

    full py code executed

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 # pkr to gbp -> 
+# Scenario 
+# usd, gbp, pkr, value 
+
+# pkr to usd -> 
 
 def pkr_to_gbp(pkr):
     rate = 300
     amount = pkr / rate
     return amount
-
-pkr = int(input("Enter the pkr amount: "))
-print(f'Your amount in USD: {pkr_to_usd(pkr)}')
-print(f'Your amount in GBP: {pkr_to_gbp(pkr)}')# Scenario 
-# usd, gbp, pkr, value 
-
-# pkr to usd -> 
 
 def pkr_to_usd(pkr):
     rate = 250
