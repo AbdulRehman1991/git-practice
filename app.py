91395e5c69aa79f39df3634daebac179caab589f
Muhammad Akbar Abdul Rehman <akbarabdulrehman25@gmail.com>
2025-04-14 14:03:08 -0700

    new py module 1

---
+++ app.py
@@ -0,0 +1,13 @@
+# Scenario 
+# usd, gbp, pkr, value 
+
+# pkr to usd -> 
+
+def pkr_to_usd(pkr):
+    rate = 250
+    amount = pkr / rate
+    return amount
+
+pkr = int(input("Enter the pkr amount: "))
+print(f'Your amount in USD: {pkr_to_usd(pkr)}')
+print(f'Your amount in GBP: {pkr_to_gbp(pkr)}')
